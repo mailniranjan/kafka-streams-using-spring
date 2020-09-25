f3637f18a6e0bc37f4a208cd09ceb4601500a626
Niranjan Ramaiah <Niranjan.Ramaiah@microfocus.com>
2020-09-25 14:26:23 +0530

    Experiment kafka streams using spring for kafka
    Here I will poll a record from one topic in kafka and do some processing
    and put in another topic in kafka. The target record will have value
    null and will have key set to the value from input topic.
    
    For ex: input topic has record key and value like (null, value). Record
    from input topic can have any key which is ignored in my logic.
    
    I will put the record (value, null) to output topic.

---
+++ kafka-streams-using-spring/src/main/java/com/microfocus/kafka/streams/demo/MyConfiguration.java
@@ -0,0 +1,59 @@
+package com.microfocus.kafka.streams.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KeyValueMapper;
+import org.apache.kafka.streams.kstream.ValueMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+
+@Configuration
+@EnableKafka
+@EnableKafkaStreams
+public class MyConfiguration
+{
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kStreamsConfigs()
+    {
+        Map<String,Object> props = new HashMap<>();
+        props.put(ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG, false);
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-demo-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "rniranjan1.epm.blr.novell.com:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        return new KafkaStreamsConfiguration(props);
+    }
+    
+    @Bean
+    public KStream<String,String> kafkaStreamWhichCopiedValueToKeyAndSetsValueAsNull(StreamsBuilder streamsBuilder)
+    {
+        KStream<String,String> stream = streamsBuilder.stream("kafka-streams-demo-input-topic");
+        stream.selectKey(new KeyValueMapper<String,String,String>()
+        {
+            @Override
+            public String apply(String key, String value)
+            {
+                return value;
+            }
+        }).mapValues(new ValueMapper<String,Object>()
+        {
+            @Override
+            public Object apply(String value)
+            {
+                //set new value as null
+                return null;
+            }
+        }).to("kafka-streams-demo-output-topic");
+        return stream;
+    }
+}
